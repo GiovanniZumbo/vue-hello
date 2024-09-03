0491afee9ea7f55f19ff6245c870aa001ad6961e
Giovanni Zumbo <gianni.zu07@gmail.com>
2024-09-03 14:59:11 +0200

    Bonus (last commit)

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const { createApp } = Vue
+const { createApp } = Vue;
 
 createApp({
 
     data() {
         return {
 
-            message: 'Prima lezione di VUE!'
-
+            message: 'Prima lezione di VUE!',
+            image: 'https://giffiles.alphacoders.com/734/7342.gif'
         }
     }
-}).mount('#app')
+}).mount('#app');
